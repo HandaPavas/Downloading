b0c77b15287ebd12821a2bf7df51f185633e0e7e
HandaPavas <pavas16040@iiitd.ac.in>
2016-11-12 05:19:36 +0530

    Second Commit

--- app/src/main/java/com/example/root/downloading/MainActivity.java
+++ app/src/main/java/com/example/root/downloading/MainActivity.java
@@ -4,105 +4,89 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.root.downloading.R;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView downloadtextview;
+    private Document Document;
+    Button downloadbutton;
+    private String Url = "https://iiitd.ac.in/about";
+    private TextView title,content;
+    private String ContentToString,TitleToString;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        downloadtextview= (TextView) findViewById(R.id.downloadtextview);
 
-    }
+        title = (TextView)findViewById(R.id.downloadtextview);
+        content = (TextView)findViewById(R.id.downloadcontent);
+        downloadbutton = (Button)findViewById(R.id.downloadbutton);
+        downloadbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // String Url = "https://iiitd.ac.in/about";
+                ConnectivityManager connectivityManager= (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 
-    public void downloading(View view){
+                if(networkInfo != null && networkInfo.isConnected()){
 
-        String Url = "https://iiitd.ac.in/about";
-        ConnectivityManager connectivityManager= (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-
-        if(networkInfo != null && networkInfo.isConnected()){
-
-            new DownloadContent().execute(Url);
-        }
-        else{
-
-            downloadtextview.setText("Nothing Downloaded Yet !!!");
-        }
-    }
-
-    private class DownloadContent extends AsyncTask<String ,Void,String> {
-        @Override
-        protected String doInBackground(String... strings) {
-            try {
-                return downloadurl(strings[0]);
-            }
-            catch (IOException e){
-
-                return "Invalid Url";
-
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            downloadtextview.setText(s);
-        }
-
-        private String downloadurl(String string) throws IOException{
-
-            InputStream inputstream= null;
-
-            int length = 200;
-            try{
-                URL url= new URL(string);
-                HttpURLConnection connection=(HttpURLConnection) url.openConnection();
-                connection.setReadTimeout(20000);
-                connection.setConnectTimeout(25000);
-                connection.getRequestMethod();
-                connection.setDoInput(true);
-                connection.connect();
-
-                int response = connection.getResponseCode();
-                Log.d("Checking response code", "The response is: " + response);
-
-                inputstream=connection.getInputStream();
-                String contentAsString = readIt(inputstream, length);
-                return contentAsString;
-
-
-
-            }finally {
-
-                if( inputstream != null){
-                    inputstream.close();
+                    JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+                    jsoupAsyncTask.execute();
                 }
+                else{
+
+                    title.setText("Nothing Downloaded Yet !!!");
+                }
+
             }
+        });
+    }
 
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
         }
 
-        private String readIt(InputStream inputstream, int length)throws IOException,UnsupportedEncodingException {
-            Reader reader = null;
-            reader = new InputStreamReader(inputstream,"UTF-8");
-            char[] buffer= new char[length];
-            reader.read(buffer);
-            return new String(buffer);
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Document = Jsoup.connect(Url).get();
+                TitleToString = Document.title();
+                ContentToString = Document.text();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
 
+        @Override
+        protected void onPostExecute(Void result) {
+            title.setText(TitleToString);
+            content.setText(ContentToString);
         }
     }
 }
